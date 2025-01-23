17c58689020a20b1a3a8658f889717ae779d12cc
SML <ronnymayamba94@gmail.com>
2025-01-23 17:34:38 +0100

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,10 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-
-
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700', '800', '900']
 })
-
-
 
 export const metadata: Metadata = {
   title: "MK Portfolio",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-export default function Home() {
+import Home from "./components/contents/home";
+
+export default function pages() {
   return (
     <main>
       <div id="home">
-
+        <Home/>
       </div>
     </main>
   );
