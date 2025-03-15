d5b4931c1294afe771ef1bf0c6cb974c0ad16f88
SML <ronnymayamba94@gmail.com>
2025-03-15 17:25:43 +0100

    fix

--- src/app/components/icon.tsx
+++ src/app/components/icon.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 
-const Icon = ({icon}) => {
+import { ReactNode } from "react";
+
+const Icon = ({ icon }: { icon: ReactNode }) => {
     return (
         <Link className=" p-7 border-solid rounded-[10px] hover:bg-white hover:text-black hover:border-[#fff] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s]" href={""}>{icon}</Link>
     )
