b021cbcd8f95fb75289fb9ccb17164b4837ed671
SML <ronnymayamba94@gmail.com>
2025-03-15 17:20:27 +0100

    add timeline composant + footer icon + react icon

---
+++ src/app/components/icon.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+const Icon = ({icon}) => {
+    return (
+        <Link className=" p-7 border-solid rounded-[10px] hover:bg-white hover:text-black hover:border-[#fff] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s]" href={""}>{icon}</Link>
+    )
+}
+
+export default Icon
