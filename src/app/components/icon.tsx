8c6a0f896655feb9a85d564d4b25fcd4a33fe482
SML <ronnymayamba94@gmail.com>
2025-03-15 17:29:37 +0100

    fix icon

--- src/app/components/icon.tsx
+++ src/app/components/icon.tsx
@@ -1,11 +1,25 @@
-import Link from "next/link"
-
-import { ReactNode } from "react";
+import Link from "next/link";
+import { ReactNode, useEffect, useState } from "react";
 
 const Icon = ({ icon }: { icon: ReactNode }) => {
-    return (
-        <Link className=" p-7 border-solid rounded-[10px] hover:bg-white hover:text-black hover:border-[#fff] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s]" href={""}>{icon}</Link>
-    )
-}
+  const [isClient, setIsClient] = useState(false);
 
-export default Icon
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // ou un spinner/loader
+  }
+
+  return (
+    <Link
+      className="p-7 border-solid rounded-[10px] hover:bg-white hover:text-black hover:border-[#fff] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s]"
+      href=""
+    >
+      {icon}
+    </Link>
+  );
+};
+
+export default Icon;
