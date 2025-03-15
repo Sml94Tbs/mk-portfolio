5e0dc818eabf97af0d42f398fa56d040b6dae8fd
SML <ronnymayamba94@gmail.com>
2025-03-15 18:09:42 +0100

    d

--- src/app/components/icon.tsx
+++ src/app/components/icon.tsx
@@ -1,23 +1,9 @@
-"use client";
+import Link from "next/link"
 
-import Link from "next/link";
-import { ReactNode, useMemo } from "react";
+const Icon = ({icon}) => {
+    return (
+        <Link className=" p-7 border-solid rounded-[10px] hover:bg-white hover:text-black hover:border-[#fff] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s]" href={""}>{icon}</Link>
+    )
+}
 
-const Icon = ({ icon }: { icon: ReactNode }) => {
-  const isClient = useMemo(() => typeof window !== "undefined", []);
-
-  if (!isClient) {
-    return null; // ou un loader
-  }
-
-  return (
-    <Link
-      className="p-7 border-solid rounded-[10px] hover:bg-white hover:text-black hover:border-[#fff] transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s]"
-      href=""
-    >
-      {icon}
-    </Link>
-  );
-};
-
-export default Icon;
+export default Icon
