4b47b058787d2ca0d7a25f3ab7c50484d2c56d7a
SML <ronnymayamba94@gmail.com>
2025-04-01 19:04:10 +0200

    add card ecole + entreprise

---
+++ src/app/components/contents/portfolio.tsx
@@ -0,0 +1,22 @@
+import CardProjet from "../cardProjet";
+
+const Portfolio = () => {
+  return (
+    <section className=" bg-[#2C3E50]">
+      <div className=" text-center">
+        <h1 className=" text-white text-[4.5rem] font-[700] leading-[1.2] mb-[5rem]">
+          Mes Réalisation Professionnelles
+        </h1>
+        <h3 className=" text-white text-[1.6rem] mb-[5rem]">
+          Retrouver tout les traveaux que j'ai pu réalisé
+        </h3>
+      </div>
+      <div className=" grid grid-cols-[repeat(2,_1fr)] items-center gap-[2.5rem] max-sm:grid-cols-[1fr]">
+        <CardProjet titre="A l'école" image="/images/saber.jpg" link="school" />
+        <CardProjet titre="En entreprise" link="/Bonjour" image="/images/saber.jpg" />
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
---
+++ src/app/school/page.tsx
@@ -0,0 +1,9 @@
+import Retour from "@/app/components/retour.button";
+
+export default function School() {
+    return(
+        <section className=" bg-[#2C3E50]">
+            <Retour link="/#portfolio" />
+        </section>
+    )
+}
