d624d9c14cdfe6c11799ffb1a9e952cccd3e8ef1
SML <ronnymayamba94@gmail.com>
2025-04-06 17:26:53 +0200

    test

--- src/app/components/veille.componenets/card.veille.tsx
+++ src/app/components/veille.componenets/card.veille.tsx
@@ -22,7 +22,7 @@ const CardVeille = ({key, title, date, desc, link}:{key: number, title : string,
           }
     }, [])
     return (
-        <Link href={link} target="_blank" className="wsh3" key={key}>
+        <Link href={link} target="_blank" key={key}>
             <div className=" bg-[#6185a8] flex justify-center text-white border-2 border-solid rounded-[1rem]  transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s] hover:scale-[1.02]">
                 <div className=" p-[1rem_3rem] w-[24.8rem]  text-center justify-center ">
                     <h2 className=" text-[2rem]  font-bold max-lg:text-[1.5rem]">
--- src/app/components/veille.componenets/listCard.veille.tsx
+++ src/app/components/veille.componenets/listCard.veille.tsx
@@ -1,14 +1,55 @@
-import CardVeille from "@/app/components/veille.componenets/card.veille"
-import { googleAlerts } from "@/app/data/googleAlerts"
+import CardVeille from "@/app/components/veille.componenets/card.veille";
+import { googleAlerts } from "@/app/data/googleAlerts";
+import { useState } from "react";
 const ListCard = () => {
-    return (
-        <div className="  grid grid-cols-[repeat(4,_1fr)] gap-[1.6rem] max-xl:grid-cols-[repeat(2,_1fr)] max-lg:grid-cols-[repeat(1,_1fr)]">
-        { googleAlerts.map((alert, index) => (
-            <CardVeille key={index} title={alert.title} date={alert.pubDate} desc={alert.snippet} link={alert.link}/>
-        ))}
-        
-        </div>
-    )
-}
+  const [selectedCategory, setSelectedCategory] = useState<
+    keyof typeof googleAlerts | "all"
+  >("all");
 
-export default ListCard
+  const allAlerts = Object.keys(googleAlerts)
+    .flat()
+
+  const filteredAlerts =
+    selectedCategory === "all"
+      ? Object.values(googleAlerts).flat()
+      : googleAlerts[selectedCategory] || [];
+
+  return (
+    <div className=" p-4 text-white">
+      {/* Button de sélection */}
+      <div className="flex justify-center flex-wrap gap-4 mb-6">
+        <button
+          className={`px-4 py-2 rounded-full border ${
+            selectedCategory === "all" ? "bg-white text-black" : "border-white"
+          }`}
+          onClick={() => setSelectedCategory("all")}
+          >
+            Tous
+          </button>
+          {allAlerts.map((index) => (
+            <button key={index} className={`px-4 py-2 rounded-full border ${
+                selectedCategory === index ? "bg-white text-black" : "border-white"
+              }`}
+              onClick={() => setSelectedCategory(index)}
+              >
+                {index}
+            </button>
+          ))}
+      </div>
+      <div className="  grid grid-cols-[repeat(4,_1fr)] gap-[1.6rem] max-xl:grid-cols-[repeat(2,_1fr)] max-lg:grid-cols-[repeat(1,_1fr)]">
+        {filteredAlerts.map((alert, index) => (
+          <CardVeille
+            key={index}
+            title={alert.title}
+            date={alert.pubDate}
+            desc={alert.snippet}
+            link={alert.link}
+          />
+        ))}
+      </div>
+      s
+    </div>
+  );
+};
+
+export default ListCard;
