27d9d2b8ba875bd3e9dcb6a515c8ff7a1e14d595
SML <ronnymayamba94@gmail.com>
2025-04-07 20:14:56 +0200

    add button

--- src/app/components/button.home.tsx
+++ src/app/components/button.home.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
 
 interface ButtonHomeProps {
-    icon: React.ReactNode;
-    link: string;
-    reseau: string;
+  icon: React.ReactNode;
+  link: string;
+  reseau: string;
 }
 
-const ButtonHome = ({ icon, link, reseau }: ButtonHomeProps) => { 
-    return (
-        <Link href={link}>
-            <div className=" group bg-black text-white flex items-center overflow-hidden gap-2 p-5 rounded-[5rem]  transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s]">
-            <i className=" text-[2.5rem]">{icon}</i>
-            <p className="text-[1.2rem] group-hover:block transition-[cubic-bezier(0.25_0.1_0.25_1.0)] duration-[.5s] ease-in-out hidden"> {reseau}</p>
-            </div>
-        </Link>
-    )
-}
+const ButtonHome = ({ icon, link, reseau }: ButtonHomeProps) => {
+  return (
+    <Link href={link} target="_blank" className="group inline-block">
+      <div className="  bg-black text-white flex items-center w-[5rem] gap-2 p-5 rounded-full group-hover:w-[12rem] overflow-hidden  transition-all duration-500 ease-in-out ">
+        <i className=" text-[2.5rem]">{icon}</i>
+        <span className="text-[1.2rem] ml-3 whitespace-nowrap font-bold group-hover:translate-x-0 opacity-0 translate-x-[-1rem] group-hover:opacity-100 transition-all duration-500 ease-in-out ">
+          {reseau}
+        </span>
+      </div>
+    </Link>
+  );
+};
 
 export default ButtonHome;
